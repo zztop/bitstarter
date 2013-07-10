c4b88ba6fb8f1df3bd3426ba1997e2d3469c56d3
zztop420 <zaryab.ahmad@gmail.com>
2013-07-10 00:12:38 -0400

    fixed sonme locked file issue

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
-var express = require('express');
+#!/usr/bin/env node
+
+"use strict";
+var express = require("express"),
+	fs = require("fs");
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get("/", function (request, response) {
+	var fileData =fs.readFileSync("./index.html","utf8");
+	response.send(fileData);
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+	console.log("Listening on " + port);
 });
